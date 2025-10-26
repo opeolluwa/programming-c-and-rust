76a3de93a4558ae674ee8b2fdd19f60e424ff038
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-10-26 21:18:05 +0100

    hello, CPP

---
+++ hello_cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int answer{42};
+    std::cout << "The answer to life, the universe and everything is "
+        << answer
+        << std::endl;
+    return 0;
+}
+2
