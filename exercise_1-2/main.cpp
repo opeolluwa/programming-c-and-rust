6990600119d013bfe0d056a21de12017a2bdb9a6
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-10-28 08:27:45 +0100

    exercise 1-2: Beginning C++17 From Novice to Professional ( PDFDrive.com ).pdf

---
+++ exercise_1-2/main.cpp
@@ -0,0 +1,14 @@
+/**
+ *
+ *exercise 1-2:
+ *Create and execute a program that outputs
+ * your name on one line and your age on the next line.
+ *
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "Name: Alex Minato\nAge: 35" << std::endl;
+    return 0;
+}
